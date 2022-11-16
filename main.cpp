d1e1627c25d35c5ebf85ff9367675114920972dc
Ebrar <ebrark@Ebrars-MacBook-Pro.local>
2022-11-16 16:09:10 +0300

    Fix some errors regarding st deviation and calculations.

--- main.cpp
+++ main.cpp
@@ -3,22 +3,24 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <chrono>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 using namespace std::chrono;
-typedef void *(*RunnerFunction) (void *param);
 
 int NUM_OF_INTS = 0;        // number of random integers
 int NUM_OF_THREADS = 0;     // number of threads
 vector<int> my_array;       // our global array
 
 // Following variables will keep the outputs:
-int min_of, max_of, range_of, mode_of, sum_of;
-double median_of, art_mean, st_deviation, harm_mean, interquartile_range;
-
+// 0- (int)min, 1- (int)max, 2- (int)range, 3- (int)mode, 4- median,
+// 5- (int)sum, 6- art_mean, 7- harm_mean, 8- st_deviation, 9 - interquartile_range
+double outputs[10];
+void print_outputs(ofstream& print_to_file, double duration);
 void find_min();
 void find_max();
 void find_range();
@@ -49,6 +51,7 @@ void *thread8(void *param);
 void *thread9(void *param);
 void *thread10(void *param);
 
+typedef void *(*RunnerFunction) (void *param);
 RunnerFunction five_functions[5] = {thread51, thread52, thread53, thread54, thread55};
 RunnerFunction ten_functions[10] = {thread1, thread2, thread3, thread4, thread5, thread6, thread7, thread8, thread9, thread10};
 
@@ -61,30 +64,41 @@ int main(int argc, char *argv[]) {
     }
     sort(my_array.begin(), my_array.end());     // Sorts in NlogN complexity
     pthread_t workers[NUM_OF_THREADS];
-
     auto start = high_resolution_clock::now();
     switch(NUM_OF_THREADS) {
         case 1: {
+            ofstream outfile ("output1.txt");
             pthread_create(&workers[0], NULL, &one_thread, NULL);   // create a thread
             pthread_join(workers[0], NULL);    // wait for the thread to exit
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            print_outputs(outfile, duration.count());
             break;
         }
         case 5: {
+            ofstream outfile ("output2.txt");
             for(int i = 0; i < NUM_OF_THREADS; i ++) {
                 pthread_create(&workers[i], NULL, five_functions[i], NULL);
             }
             for(int i = 0; i < NUM_OF_THREADS; i ++) {
                 pthread_join(workers[i], NULL);
             }
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            print_outputs(outfile, duration.count());
             break;
         }
         case 10: {
+            ofstream outfile ("output3.txt");
             for(int i = 0; i < NUM_OF_THREADS; i ++) {
                 pthread_create(&workers[i], NULL, ten_functions[i], NULL);
             }
             for(int i = 0; i < NUM_OF_THREADS; i ++) {
                 pthread_join(workers[i], NULL);
             }
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            print_outputs(outfile, duration.count());
             break;
         }
         default: {
@@ -92,15 +106,22 @@ int main(int argc, char *argv[]) {
             break;
         }
     }
-    cout << min_of << "\n" << max_of << "\n" <<
-            range_of << "\n" << mode_of << "\n" <<
-            median_of << "\n" << sum_of << "\n" <<
-            art_mean << "\n" << harm_mean << "\n" <<
-            st_deviation << "\n" << interquartile_range << "\n";
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << duration.count()/(1e6) << "\n";
     return 0;
+}
+
+void print_outputs(ofstream& print_to_file, double duration) {
+    print_to_file << setprecision(5) << fixed;
+    for(int i = 0; i < 10; i++) {
+    double result = outputs[i];
+        if(result - (int) result < 0.00001) {
+            print_to_file << (int)result << "\n";
+        }
+        else{
+            print_to_file << result << "\n";
+        }
+    }
+    print_to_file << duration/(1e6) << "\n";
+    print_to_file.close();
 }
 
 void *one_thread( void *param ) {
@@ -109,8 +130,6 @@ void *one_thread( void *param ) {
     find_range();
     find_mode();
     find_median();
-    find_sum();
-    find_arithmetic_mean();
     find_harmonic_mean();
     find_st_deviation();
     find_interquartile_range();
@@ -130,7 +149,6 @@ void *thread52(void *param) {   // range and median
 }
 
 void *thread53(void *param) {   // sum and art_mean
-    find_sum();
     find_arithmetic_mean();
     pthread_exit(0);
 }
@@ -144,12 +162,14 @@ void *thread54(void *param) {   // mode, inter_range
 void *thread55(void *param) {   // harm_mean, st_deviation
     double harm_denominator = 0;
     double st_numerator = 0;
+    find_arithmetic_mean();
+    double art_mean = outputs[6];
     for(int i = 0; i < NUM_OF_INTS; i ++) {
         harm_denominator += (double)1/(double)my_array.at(i);
         st_numerator += pow(my_array.at(i) - art_mean, 2);
     }
-    harm_mean = (double)NUM_OF_INTS / harm_denominator;
-    st_deviation = pow(st_numerator / (double)(NUM_OF_INTS-1), 0.5);
+    outputs[7] = (double)NUM_OF_INTS / harm_denominator;
+    outputs[8] = pow(st_numerator / (double)(NUM_OF_INTS-1), 0.5);
     pthread_exit(0);
 }
 
@@ -204,24 +224,24 @@ void *thread10(void *param) {
 }
 
 void find_min() {    // O(1)
-    min_of = my_array.at(0);
+    outputs[0] = my_array.at(0);
     return;
 }
 
 void find_max() {    // O(1)
-    max_of = my_array.at(NUM_OF_INTS - 1);
+    outputs[1] = my_array.at(NUM_OF_INTS - 1);
     return;
 }
 
 void find_range() {    // O(1)
-    range_of = my_array.at(NUM_OF_INTS - 1) - my_array.at(0);
+    outputs[2] = my_array.at(NUM_OF_INTS - 1) - my_array.at(0);
     return;
 }
 
 void find_mode() {      // O(N)
     int max_count = 0;
     int count = 1;
-    mode_of = my_array.at(0);
+    int mode_of = my_array.at(0);
     for(int i = 0; i < NUM_OF_INTS - 1; i ++) {
         if(my_array.at(i) == my_array.at(i+1)) {
             count++;
@@ -234,30 +254,32 @@ void find_mode() {      // O(N)
             count = 1;
         }
     }
+    outputs[3] = mode_of;
     return;
 }
 
 void find_median() {     // O(1)
     if(NUM_OF_INTS % 2 == 1) {  // odd
-        median_of = my_array.at(NUM_OF_INTS/2);
+        outputs[4] = my_array.at(NUM_OF_INTS/2);
     }
     else{   // even
-        median_of = ( (double)my_array.at(NUM_OF_INTS/2) + (double)my_array.at(NUM_OF_INTS/2 - 1) ) / 2;
+        outputs[4] = ( (double)my_array.at(NUM_OF_INTS/2) + (double)my_array.at(NUM_OF_INTS/2 - 1) ) / 2;
     }
     return;
 }
 
 void find_sum() {    // O(N)
-    sum_of = 0;
+    int sum = 0;
     for(int i = 0; i < NUM_OF_INTS; i ++) {
-        sum_of += my_array.at(i);
+        sum += my_array.at(i);
     }
+    outputs[5] = sum;
     return;
 }
 
 void find_arithmetic_mean() {      // O(1)
-    // depends on find_sum()
-    art_mean = (double) sum_of / NUM_OF_INTS;
+    find_sum();     // arithmetic mean calculation depends on the sum
+    outputs[6] = (double) outputs[5] / NUM_OF_INTS;
     return;
 }
 
@@ -266,16 +288,18 @@ void find_harmonic_mean() {        // O(N)
     for(int i = 0; i < NUM_OF_INTS; i ++) {
         denominator += (double)1/(double)my_array.at(i);
     }
-    harm_mean = (double)NUM_OF_INTS / denominator;
+    outputs[7] = (double)NUM_OF_INTS / denominator;
     return;
 }
 
 void find_st_deviation() {        // O(N)
     double numerator = 0;
+    find_arithmetic_mean();     // standard deviation calculation depends on the arithmetic mean
+    double art_mean = outputs[6];
     for(int i = 0; i < NUM_OF_INTS; i ++) {
         numerator += pow(my_array.at(i) - art_mean, 2);
     }
-    st_deviation = pow(numerator / (double)(NUM_OF_INTS-1), 0.5);
+    outputs[8] = pow(numerator / (double)(NUM_OF_INTS-1), 0.5);
     return;
 }
 
@@ -290,6 +314,6 @@ void find_interquartile_range() {     // O(1)
         right_median = (double)(my_array.at(NUM_OF_INTS - half/2) + my_array.at(NUM_OF_INTS - half/2 - 1))/2;
         left_median =  (double)(my_array.at(half/2) + my_array.at(half/2 - 1))/2;
     }
-    interquartile_range = right_median - left_median;
+    outputs[9] = right_median - left_median;
     return;
 }
